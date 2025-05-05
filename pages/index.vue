24206979d68e13afb509b1d320fdcdcd42184536
dont1000 <work@braitling.de>
2025-05-05 21:19:21 +0200

    optimize for mobile

--- pages/index.vue
+++ pages/index.vue
@@ -6,4 +6,10 @@
 
 <script setup>
 import Chatbot from "~/components/Itsralf.vue"
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+  ],
+  title: 'Chat'
+})
 </script>
